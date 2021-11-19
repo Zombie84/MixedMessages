707dc8313db2e82e9d7240631d9df94545252714
Zombie84 <91849082+Zombie84@users.noreply.github.com>
2021-11-20 00:03:15 +0100

    word-arrays now can have various lengths

--- mixedMassages.js
+++ mixedMassages.js
@@ -1,15 +1,19 @@
 function getMessage() {
-    let part_1_words= ['Hermann', 'Odin', 'Freya', 'Heimdall', 'Thor', 'Loki'];
+    let part_1_words= ['Hermann', 'Odin', 'Freya', 'Heimdall', 'Thor', 'Loki', 'Seppl', 'Knight', 'Monster', 'MalevolentCreation'];
     let part_2_words= ['shoots', 'slays', 'drowns', 'kills', 'beats', 'tramples'];
     let part_3_words= ['weakling', 'liar', 'wimp', 'idiot', 'traitor', 'fool'];
     let words= [part_1_words, part_2_words, part_3_words];
     let message= '';
     let rand;
 
-    for (let i=0; i<3; i++){
-        rand= Math.floor(Math.random()*6);
+    for (let i=0; i<words.length; i++){
+        rand= Math.floor(Math.random()*(words[i].length));
         message += words[i][rand] + ' ';
         // console.log(i, rand);
     }
     return message;
 };
+
+for (let j=0; j<10; j++) {
+    console.log(getMessage());
+}
